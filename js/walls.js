73fc2cf9c4004d2c581be7982ad228394f6bfc5b
nataliawas <nataliawas@wp.pl>
2018-12-20 18:48:32 +0100

    for loop fixed

--- js/walls.js
+++ js/walls.js
@@ -12,8 +12,8 @@ function randomNumber() {
 function getCoordinates() {
     do {
         var coordinates = []
-        coordinates.push(createCoordinates(randomNumber(), randomNumber()))
-        for (var i = 0; i < 39; i++) {
+        
+        for (var i = 0; i < 40; i++) {
 
             if (coordinates.indexOf(createCoordinates(randomNumber(), randomNumber())) === -1) {
                 coordinates.push(createCoordinates(randomNumber(), randomNumber()))
