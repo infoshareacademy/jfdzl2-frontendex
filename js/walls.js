fc4f022c45e1dd54cfe7fdba4644b7855ae86036
Nataliawas <nataliawas@wp.plgit config --user.name Nataliawasgit config --global user.email nataliawas@wp.pl>
2019-01-15 18:46:13 +0100

    changes in code

--- js/walls.js
+++ js/walls.js
@@ -1,37 +1,84 @@
-function createCoordinates(x, y) {
-    return {
-        x: x,
-        y: y
+// function createCoordinates(x, y) {
+//     return {
+//         x: x,
+//         y: y
+//     }
+// }
+
+// function randomNumber() {
+//     return Math.floor(Math.random() * 20)
+// }
+
+// function getCoordinates() {
+//     do {
+//         var coordinates = []
+
+//         for (var i = 0; i < 40; i++) {
+
+//             if (coordinates.indexOf(createCoordinates(randomNumber(), randomNumber())) === -1) {
+//                 coordinates.push(createCoordinates(randomNumber(), randomNumber()))
+//             }
+//         }
+//         return coordinates
+//     }
+//     while (coordinates.length === 40)
+// }
+
+// function renderWalls() {
+//     var walls = getCoordinates()
+//     var wallsContent = document.querySelector('.board')
+//     wallsContent.innerHTML = ''
+
+//     walls.forEach(function (coordinate) {
+//         var content = `<div class="wall" style="left: ${coordinate.x * 5}%; top: ${coordinate.y * 5}%;" ></div>`
+
+//         var wall = wallsContent.insertAdjacentHTML('afterbegin', content)
+//     })
+// }
+
+class WallCoordinates {
+    constructor() {
+        this.x = x
+        this.y = y
     }
-}
 
-function randomNumber() {
-    return Math.floor(Math.random() * 20)
-}
+    randomNumber() {
+        return Math.floor(Math.random() * 20)
+    }
 
-function getCoordinates() {
-    do {
-        var coordinates = []
+    getCoordinates() {
+        do {
+            const coordinates = []
 
-        for (var i = 0; i < 40; i++) {
+            for (i = 0; i < 40; i++) {
 
-            if (coordinates.indexOf(createCoordinates(randomNumber(), randomNumber())) === -1) {
-                coordinates.push(createCoordinates(randomNumber(), randomNumber()))
+                if (coordinates.indexOf(createCoordinates(randomNumber(), randomNumber())) === -1) {
+                    coordinates.push(createCoordinates(randomNumber(), randomNumber()))
+                }
             }
+            return coordinates
         }
-        return coordinates
+        while (coordinates.length === 40)
     }
-    while (coordinates.length === 40)
-}
 
-function renderWalls() {
-    var walls = getCoordinates()
-    var wallsContent = document.querySelector('.board')
-    wallsContent.innerHTML = ''
+    renderWalls() {
+        const walls = getCoordinates()
+        const wallsContent = document.querySelector('.board')
+        wallsContent.innerHTML = ''
 
-    walls.forEach(function (coordinate) {
-        var content = `<div class="wall" style="left: ${coordinate.x * 5}%; top: ${coordinate.y * 5}%;" ></div>`
+        walls.forEach(function (coordinate) {
+            const wall = document.createElement('div')
+            wall.classList.add('wall')
 
-        var wall = wallsContent.insertAdjacentHTML('afterbegin', content)
-    })
+            wall.style.left = this.x * 5 + '%';
+            wall.style.top = this.y * 5 + '%';
+
+            const board = document.querySelector('.board')
+            board.appendChild(wall)
+        })
+    }
+    init() {
+        this.renderWalls()
+    }
+
 }
