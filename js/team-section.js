0a4a31677877e0acfec8c66ac591ce67803a2bff
emkozuch <emkozuch@gmail.com>
2019-01-20 12:20:19 +0100

    added js file

---
+++ js/team-section.js
@@ -0,0 +1,10 @@
+const flippedAvatar = document.querySelectorAll('.flip-avatar')
+
+    for (let i = 0; i < flippedAvatar.length; i++){
+        // flippedAvatar[i].classList.toggle('animated')
+    }
+
+
+    // const section = document.getElementById('')
+
+
