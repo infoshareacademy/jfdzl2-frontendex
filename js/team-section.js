8c4f70e6fa15d60c95d70f15849313e5f829b1cf
emkozuch <emkozuch@gmail.com>
2019-01-20 13:18:06 +0100

    js update

--- js/team-section.js
+++ js/team-section.js
@@ -1,10 +1,25 @@
 const flippedAvatar = document.querySelectorAll('.flip-avatar')
 
+    // for (let i = 0; i < flippedAvatar.length; i++){
+    //     // flippedAvatar[i].classList.toggle('animated')
+    // }
+
+
+   
+  
+    // console.log(bounding)
+
+    const isInViewport = function (section) {
+        const bounding = section.getBoundingClientRect();
+        return (bounding.top >= 0 && bounding.left >= 0 && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) && bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    };
+
+    const teamSection = document.getElementById('team')
+
+
     for (let i = 0; i < flippedAvatar.length; i++){
-        // flippedAvatar[i].classList.toggle('animated')
+        if (isInViewport(teamSection)) {
+        flippedAvatar[i].classList.toggle('animated')
     }
-
-
-    // const section = document.getElementById('')
-
-
+}
