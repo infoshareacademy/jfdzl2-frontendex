ed85769fbe313c85a3bc050ff4b3bf1d8a912e70
emkozuch <emkozuch@gmail.com>
2019-01-24 18:45:32 +0100

    opponent-render

---
+++ js/game-opponent.js
@@ -0,0 +1,54 @@
+class Opponent {
+    constructor(walls) {
+        this.walls = walls;
+        this.size = 20;
+        this.position = { x: 0, y: 0 }
+    }
+
+    getRandomNumbers() {
+        return Math.floor(Math.random() * 20);
+    }
+
+    checkOpponentWallsCollision(){
+        
+        const filteredWalls = this.walls.walls.filter(function (wall) {
+            return wall.position.x === x && wall.position.y === y;
+        });
+
+        const isMatch = filteredWalls.length > 0;
+        return isMatch;
+
+    }
+
+    setRandomPosition() {
+        const x = this.getRandomNumbers();
+        const y = this.getRandomNumbers();
+        this.position.x = x;
+        this.position.y = y;
+    }
+    randomMovement() {
+       
+    }
+    opponentRender() {
+        const opponentDOM = document.createElement('div');
+        const boardDOM = document.querySelector('.board');
+
+        opponentDOM.classList.add('opponent');
+        opponentDOM.style.left = this.position.x * 5 + '%';
+        opponentDOM.style.top = this.position.y * 5 + '%';
+
+        boardDOM.appendChild(opponentDOM);
+    }
+    init() {
+        this.setRandomPosition();
+        
+        if (this.checkOpponentWallsCollision()) {
+            this.setRandomPosition();
+        }
+        this.opponentRender();
+        this.checkOpponentWallsCollision()
+        
+    }
+}
+const opponent = new Opponent;
+opponent.init()
