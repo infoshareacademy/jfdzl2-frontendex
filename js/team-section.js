b6f2fb9e9a37b523eeca1b519ceabec417b59d39
Marcin <emkozuch@gmail.com>
2019-03-10 00:25:46 +0100

    animation on scroll

--- js/team-section.js
+++ js/team-section.js
@@ -1,34 +1,24 @@
-// const flippedAvatar = document.querySelectorAll('.front')
-// const teamSection = document.getElementById('team')
+const avatar = document.querySelectorAll('.image-container')
+const teamSection = document.getElementById('team')
 
-// const isInViewport = function (section) {
-//         // const bounding = section.getBoundingClientRect();
-//         // return (bounding.top >= 0 && bounding.left >= 0 && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) && bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-//         // );
-//         var top = section.offsetTop;
-//   var left = section.offsetLeft;
-//   var width = section.offsetWidth;
-//   var height = section.offsetHeight;
+const isInViewport = (section) => {
+    const bounding = section.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
 
-//   while(section.offsetParent) {
-//     section = section.offsetParent;
-//     top += section.offsetTop;
-//     left += section.offsetLeft;
-//   }
+ const animateAvatars = () => {
+    window.addEventListener('scroll', () => {
+        if (isInViewport(teamSection)) {
+            for (let i = 0; i < avatar.length; i++) {
+                avatar[i].classList.add('animated', 'rubberBand')
+            }
+        }
+    }, false);
+}
 
-//   return (
-//     top >= window.pageYOffset &&
-//     left >= window.pageXOffset &&
-//     (top + height) <= (window.pageYOffset + window.innerHeight) &&
-//     (left + width) <= (window.pageXOffset + window.innerWidth)
-//   );
-//     };
-
-//     for (let i = 0; i < flippedAvatar.length; i++){
-//         if (isInViewport(teamSection)) {
-//         flippedAvatar[i].classList.add('animated')
-//     } else if (!isInViewport(teamSection)){
-//         flippedAvatar[i].classList.remove('animated')
-//     }
-// }
-
+animateAvatars();
