7157b5fe5565a8c2756addc5d3e5563d607a965d
Marcin <emkozuch@gmail.com>
2019-03-09 21:56:01 +0100

    social media on back

--- js/team-section.js
+++ js/team-section.js
@@ -1,25 +1,34 @@
-const flippedAvatar = document.querySelectorAll('.flip-avatar')
+// const flippedAvatar = document.querySelectorAll('.front')
+// const teamSection = document.getElementById('team')
 
-    // for (let i = 0; i < flippedAvatar.length; i++){
-    //     // flippedAvatar[i].classList.toggle('animated')
-    // }
+// const isInViewport = function (section) {
+//         // const bounding = section.getBoundingClientRect();
+//         // return (bounding.top >= 0 && bounding.left >= 0 && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) && bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+//         // );
+//         var top = section.offsetTop;
+//   var left = section.offsetLeft;
+//   var width = section.offsetWidth;
+//   var height = section.offsetHeight;
 
+//   while(section.offsetParent) {
+//     section = section.offsetParent;
+//     top += section.offsetTop;
+//     left += section.offsetLeft;
+//   }
 
-   
-  
-    // console.log(bounding)
+//   return (
+//     top >= window.pageYOffset &&
+//     left >= window.pageXOffset &&
+//     (top + height) <= (window.pageYOffset + window.innerHeight) &&
+//     (left + width) <= (window.pageXOffset + window.innerWidth)
+//   );
+//     };
 
-    const isInViewport = function (section) {
-        const bounding = section.getBoundingClientRect();
-        return (bounding.top >= 0 && bounding.left >= 0 && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) && bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    };
+//     for (let i = 0; i < flippedAvatar.length; i++){
+//         if (isInViewport(teamSection)) {
+//         flippedAvatar[i].classList.add('animated')
+//     } else if (!isInViewport(teamSection)){
+//         flippedAvatar[i].classList.remove('animated')
+//     }
+// }
 
-    const teamSection = document.getElementById('team')
-
-
-    for (let i = 0; i < flippedAvatar.length; i++){
-        if (isInViewport(teamSection)) {
-        flippedAvatar[i].classList.toggle('animated')
-    }
-}
